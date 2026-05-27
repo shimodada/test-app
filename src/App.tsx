7d83064f4cd4391953ebcb47ed39b61f1b6d5293
shimodayuki <yuki.prmswr1996@gmail.com>
2026-05-28 04:05:21 +0900

    最初の一歩

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,7 @@ function App() {
   return (
     <div className="App">
       <main className="App-main">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          これはテスト用の画面です。
       </main>
     </div>
   );
